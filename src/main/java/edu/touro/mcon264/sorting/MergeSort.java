b3c2e64445dad011264f28b2b68b76b40d6425c2
SKIND0 <skind@student.touro.edu>
2026-01-05 17:03:29 -0500

    done

--- src/main/java/edu/touro/mcon264/sorting/MergeSort.java
+++ src/main/java/edu/touro/mcon264/sorting/MergeSort.java
@@ -1,11 +1,49 @@
 package edu.touro.mcon264.sorting;
 
-import java.util.Comparator;
+import java.util.*;
 
 public class MergeSort implements Sorter {
 
     @Override
     public <T> void sort(T[] a, Comparator<? super T> comp) {
-        // TODO: implement merge sort
+        if (a.length <= 1) {
+            return;
+        }
+        int mid = a.length / 2;
+        T[] left = Arrays.copyOfRange(a, 0, mid);
+        T[] right = Arrays.copyOfRange(a, mid, a.length);
+
+        sort(left, comp);
+        sort(right, comp);
+        merge(a, left, right, comp);
+    }
+
+    private <T> void merge(T[] a, T[] left, T[] right, Comparator<? super T> comp) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (comp.compare(left[i], right[j]) <= 0) {
+                a[k] = left[i];
+                i++;
+            } else {
+                a[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < left.length) {
+            a[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while (j < right.length) {
+            a[k] = right[j];
+            j++;
+            k++;
+        }
     }
 }
